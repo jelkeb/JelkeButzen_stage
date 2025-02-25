83ab5403ba89e87dbb1a19b95ec020a65b1572f4
Jelke Butzen <jelkebutzen@air-van-jelke.uantwerpen.be>
2025-02-25 15:29:43 +0100

    add R code

--- Risk_test_.R
+++ Risk_test_.R
@@ -1,15 +1,15 @@
 install.packages("readr")
 library(readr)
-df<-read_tsv("Documents/stage/risks_tests.tsv")
+df<-read_tsv("risks_tests_20.tsv")
 
 
 #verschil berekenen tussen beide cognitieve testen
-df$verschil_cogn_test<- df$`MMSE 1.y`- df$`MOCA 3 (per 21/01)`
+df$verschil_cogn_test<- df$`MMSE`- df$`MOCA`
 
 
 
 #verschil berekenen tussen data van afname cognitieve test
-df$verschil_dag_testafn <- df$`MOCA date`- df$`MMSE date`
+df$verschil_dag_testafn <- df$`MOCA_date`- df$`MMSE_date`
 
 #verschil van dagen naar jaren zetten
 df$verschil_jaar_testafn <- (df$`verschil_dag_testafn`/(365))
@@ -22,11 +22,12 @@ df$verschil_jaar_testafn <- as.numeric(df$verschil_jaar_testafn)
 #daling cognitieve test per jaar
 df$decrease_by_year <- (df$verschil_cogn_test/ (df$verschil_jaar_testafn))
 
+library(tidyverse)
 
 
 #grafiek maken: cognitieve achteruitgang linken met BP (ja/nee)
 df_selected <- df |>
-  filter(`MOCA 3 (per 21/01)`<26, verschil_cogn_test>=3)
+  filter(`MOCA`<26, `verschil_cogn_test`>=3)
 
 df_selected |> 
   ggplot(aes(x = factor(blood_pressure), y = decrease_by_year)) +
@@ -49,19 +50,59 @@ df_selected |>
   )
 
 #alle boxplotten tesamen om te kijken welke RF eruit springt
-df_selected |> 
+df_tesamen<- df|> 
   select(hearing_loss, cholesterol, education, depression, social_contact, 
          brain_injury, sight, exercise, diabetes, smoking, blood_pressure, 
-         obese) |>
-  gather(key = "risicofactoren", value = "waarde") |> 
+         obese, decrease_by_year) |>
+  gather(key = "risicofactoren", value = "waarde", -decrease_by_year) |> 
   filter(waarde == 1)
 
-df_selected |> 
 
-  
-  
-df |> 
-  ggplot(aes(x=decrease_by_year,y))
-  geom_histogram()
+ggplot(df_tesamen, aes(x = risicofactoren, y = decrease_by_year)) +
+  geom_boxplot() + 
+  labs(
+    title = "Influence of RF on cognetive tests",
+       x = "Risk factors",
+       y = "Decrease by year") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
+#GRAFIEK MAKEN
+library(ggplot2)
+
+ggplot(df, aes(x = decrease_by_year)) +
+  geom_histogram(color = "black", fill= "grey", binwidth=0.1)+
+  geom_line(data = df[df$blood_pressure == 1, ], 
+            aes(y = ..count..), stat = "bin", color = "purple", size = 1)+
+  geom_line(data = df[df$cholesterol == 1, ], 
+            aes(y = ..count..), stat = "bin", color = "yellow", size = 1)+
+  geom_line(data = df[df$obese == 1, ], 
+            aes(y = ..count..), stat = "bin", color = "red", size = 1)+
+  geom_line(data = df[df$sight == 1, ], #1 of -1??
+            aes(y = ..count..), stat = "bin", color = "blue", size = 1)+
+  geom_line(data = df[df$hearing_loss == 1, ], 
+            aes(y = ..count..), stat = "bin", color = "pink", size = 1)+
+  geom_line(data = df[df$education == -1, ], #-1 want willen degene met lage eduation
+            aes(y = ..count..), stat = "bin", color = "green", size = 1)+
+  labs(
+    x = "Average cognitive deterioration per year",
+    y = "Number of people ", 
+    color = "blood pressure"
+  )
+
+
+#nu selecteren op mensen met score lager dan 26 en verschil >3: 
+library(ggplot2)
+
+ggplot(df_selected, aes(x = decrease_by_year)) +
+  geom_histogram(color = "black", fill= "grey", binwidth=0.1, na.rm = TRUE)+
+  geom_line(data = df_selected[df_selected$hearing_loss == 1, ], 
+            aes(y = ..count..), stat = "bin", color = "pink", size = 1)+
+  geom_line(data = df_selected[df_selected$cholesterol == 1, ], 
+            aes(y = ..count..), stat = "bin", color = "yellow", size = 1)+
+  geom_line(data = df_selected[df_selected$sight == 1, ], #1 of -1??????
+            aes(y = ..count..), stat = "bin", color = "blue", size = 1)+
+  labs(
+    x = "Average cognitive deterioration per year",
+    y = "Number of people ", 
+  )
